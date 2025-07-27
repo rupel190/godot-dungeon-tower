8237e48458886bce076647c99ad58ea6d2cb952c
meister <90567661+meister56@users.noreply.github.com>
2025-07-27 16:44:35 +0200

    streamlined spawn hint management

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,26 +1,20 @@
 extends Node3D
 class_name Map
 
+#PRESETS.
 @export var debug_hints:bool = false
 @export var gridmap:GridMap
-const tower_spawn_hintname:String = "TowerSpawnDev"
-const player_spawn_hintname:String = "PlayerSpawnDev"
-const enemy_spawn_hintname:String= "EnemySpawnDev"
+var spawn_hints:Array[String] = ["PlayerSpawnDev","EnemySpawnDev","TowerSpawnDev"]
 
 
-#CACHE
-var spawns:Dictionary[String,Array]
 
-# Called when the node enters the scene tree for the first time.
-func _enter_tree() -> void:
-	initialize_spawns()
+
+
 func initialize_spawns():
-	find_and_remove_gridmap_devhints(player_spawn_hintname,"add_spawn")
-	find_and_remove_gridmap_devhints(tower_spawn_hintname,"add_spawn")
-	find_and_remove_gridmap_devhints(enemy_spawn_hintname,"add_spawn")
-	
+	for hint in spawn_hints:
+		find_and_remove_gridmap_devhints(hint,"add_spawn")
 	print(spawns)
-func find_and_remove_gridmap_devhints(hintname:String = player_spawn_hintname,hint_function:String = "add_spawn"):
+func find_and_remove_gridmap_devhints(hintname:String = spawn_hints[0],hint_function:String = "add_spawn"):
 	print(gridmap.mesh_library.get_item_list())
 	var hint_id:int = gridmap.mesh_library.find_item_by_name(hintname)
 	for cell_pos in gridmap.get_used_cells_by_item(hint_id):
@@ -34,7 +28,11 @@ func add_spawn(hint_type:String,global_pos:Vector3):
 		spawns[hint_type] = []
 	spawns[hint_type].append(global_pos)
 
+
+
+
+#CACHE
+var spawns:Dictionary[String,Array]
+
 func _ready() -> void:
-	
-	
-	pass
+	initialize_spawns()
